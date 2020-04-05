66fe86c9a45d5c9c0ffd50dc93f7d5ba1ffae5c3
whoover <whoover@nemours.org>
2020-04-05 09:12:44 -0700

    [CI]: Auto-detect lsb version when installing the MySQL drivers

--- test/fixtures/mysql/install.sh
+++ test/fixtures/mysql/install.sh
@@ -2,11 +2,11 @@
 
 # ------------------- MySQL -------------------
 
-export MYSQL_MAJOR="8"
-export MYSQL_MINOR="14"
-export MYSQL_PATCH="1"
-export MYSQL_VER="$MYSQL_MAJOR.$MYSQL_MINOR-$MYSQL_PATCH"
-export MYSQL_NAME="mysql-apt-config_0.${MYSQL_VER}_all.deb"
+MYSQL_MAJOR="8"
+MYSQL_MINOR="14"
+MYSQL_PATCH="1"
+MYSQL_VER="$MYSQL_MAJOR.$MYSQL_MINOR-$MYSQL_PATCH"
+MYSQL_NAME="mysql-apt-config_0.${MYSQL_VER}_all.deb"
 
 echo "Installing MySQL $MYSQL_VER"
 
@@ -26,16 +26,17 @@ mysql --version
 
 echo "Installed MySQL $MYSQL_VER"
 
-# ------------------- MySQL ODBC Drivers -------------------
+# ------------------- MySQL ODBC Drivers (Ubuntu) -------------------
 
 # Ubuntu version needs to match the distibution being used
-export MYSQL_UBUNTU_VER="19.10"
+MYSQL_UBUNTU_VER=`lsb_release -sr`
+MYSQL_UBUNTU_VER=`echo $MYSQL_UBUNTU_VER | sed -e 's/^[[:space:]]*//'`
 
-export MYSQL_ODBC_MAJOR="8"
-export MYSQL_ODBC_MINOR="0"
-export MYSQL_ODBC_PATCH="19"
-export MYSQL_ODBC_VER="$MYSQL_ODBC_MAJOR.$MYSQL_ODBC_MINOR.$MYSQL_ODBC_PATCH"
-export MYSQL_ODBC_NAME="mysql-connector-odbc-$MYSQL_ODBC_VER-linux-ubuntu$MYSQL_UBUNTU_VER-x86-64bit"
+MYSQL_ODBC_MAJOR="8"
+MYSQL_ODBC_MINOR="0"
+MYSQL_ODBC_PATCH="19"
+MYSQL_ODBC_VER="$MYSQL_ODBC_MAJOR.$MYSQL_ODBC_MINOR.$MYSQL_ODBC_PATCH"
+MYSQL_ODBC_NAME="mysql-connector-odbc-$MYSQL_ODBC_VER-linux-ubuntu$MYSQL_UBUNTU_VER-x86-64bit"
 
 echo "Installing MySQL ODBC driver $MYSQL_ODBC_VER"
 
